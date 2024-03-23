bd5378ebebe0e1f2f336f9b328cd1ec78b994053
Gospel011 <ugochukwugospeli111@gmail.com>
2024-03-23 11:59:28 +0100

    Setup project structure

---
+++ poll_app/lib/data_source/poll_source.dart
@@ -0,0 +1,24 @@
+
+class PollSource {
+  static Map<String, dynamic> pollData = {
+    "text": "What is your preferred platform for cross-platform mobile development?",
+    "options": [
+      {
+        "option": "Flutter",
+        "votes": 50,
+        "voted": false,
+      },
+      {
+        "option": "React Native",
+        "votes": 13,
+        "voted": false,
+      },
+      {
+        "option": "Angular & Ionic",
+        "votes": 9,
+        "voted": false,
+      },
+      
+    ],
+  };
+}
